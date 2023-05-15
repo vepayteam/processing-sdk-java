cce529c2b1e1f6047bf15f770416c4963a8c2ef7
Siarhei Yartsau <syarcev@vepay.online>
2023-05-15 17:40:13 +0300

    https://it.dengisrazy.ru/browse/MON-68 feat: Добавил фичу по получение sse сообщений от сервера. Оставил тестовый URL для проверки работоспособности

--- MoneyTransferSDK/src/main/java/io/finbridge/vepay/moneytransfersdk/presentation/fragments/threeds/ThreeDSViewModel.kt
+++ MoneyTransferSDK/src/main/java/io/finbridge/vepay/moneytransfersdk/presentation/fragments/threeds/ThreeDSViewModel.kt
@@ -1,5 +1,8 @@
 package io.finbridge.vepay.moneytransfersdk.presentation.fragments.threeds
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ThreeDSViewModel : ViewModel() {}
+@HiltViewModel
+class ThreeDSViewModel @Inject constructor() : ViewModel() {}
