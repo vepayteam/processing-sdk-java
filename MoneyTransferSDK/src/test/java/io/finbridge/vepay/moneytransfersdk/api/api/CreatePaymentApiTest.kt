11dfeb83d436a35530ee9728858060b16cea27a6
Siarhei Yartsau <syarcev@vepay.online>
2024-02-26 10:17:55 +0300

    feat: MON-487 Поправил тесты

--- MoneyTransferSDK/src/test/java/io/finbridge/vepay/moneytransfersdk/api/api/CreatePaymentApiTest.kt
+++ MoneyTransferSDK/src/test/java/io/finbridge/vepay/moneytransfersdk/api/api/CreatePaymentApiTest.kt
@@ -1,23 +1,23 @@
 package io.finbridge.vepay.moneytransfersdk.api.api
 
-import io.finbridge.vepay.moneytransfersdk.api.GetPaymentApi
+import io.finbridge.vepay.moneytransfersdk.api.CreatePaymentApi
 import io.finbridge.vepay.moneytransfersdk.api.utils.DataMocks
-import io.finbridge.vepay.moneytransfersdk.api.utils.GetPaymentResponse
+import io.finbridge.vepay.moneytransfersdk.api.utils.CreatePaymentResponse
 import io.finbridge.vepay.moneytransfersdk.api.utils.KtorUtils
 import io.finbridge.vepay.moneytransfersdk.data.repository.Response
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
 
-internal class GetPaymentApiTest {
-    private val getPaymentApiTest =
-        GetPaymentApi(KtorUtils.provideKtor(GetPaymentResponse))
+internal class CreatePaymentApiTest {
+    private val getCreatePaymentApiTest =
+        CreatePaymentApi(KtorUtils.provideKtor(CreatePaymentResponse))
 
     @Test
     fun execute() {
         runBlocking {
             val expectResponse = DataMocks.paymentResponse
-            val actualResponse = getPaymentApiTest.execute("id")
+            val actualResponse = getCreatePaymentApiTest.execute("id","123", DataMocks.paymentRequest)
 
             Assert.assertTrue(expectResponse == (actualResponse as Response.Success).result)
         }
--- MoneyTransferSDK/src/test/java/io/finbridge/vepay/moneytransfersdk/data/repository/PaymentRepositoryImplTest.kt
+++ MoneyTransferSDK/src/test/java/io/finbridge/vepay/moneytransfersdk/data/repository/PaymentRepositoryImplTest.kt
@@ -1,7 +1,6 @@
 package io.finbridge.vepay.moneytransfersdk.data.repository
 
 import io.finbridge.vepay.moneytransfersdk.api.CreatePaymentApi
-import io.finbridge.vepay.moneytransfersdk.api.GetPaymentApi
 import io.finbridge.vepay.moneytransfersdk.api.utils.DataMocks
 import io.mockk.coVerify
 import io.mockk.mockk
@@ -10,15 +9,15 @@ import org.junit.Test
 
 class PaymentRepositoryImplTest {
     private val createPaymentApi: CreatePaymentApi = mockk(relaxed = true)
-    private val getPaymentApi: GetPaymentApi = mockk(relaxed = true)
-    private val repository = PaymentRepositoryImpl(createPaymentApi, getPaymentApi)
+    private val repository = PaymentRepositoryImpl(createPaymentApi)
 
     @Test
     fun createPayment() = runBlocking {
         val id = "id"
+        val xUser = "xUser"
         val request = DataMocks.paymentRequest
 
-        repository.createPayment(id, request)
-        coVerify(exactly = 1, verifyBlock = { createPaymentApi.execute(id, request) })
+        repository.createPayment(id, xUser, request)
+        coVerify(exactly = 1, verifyBlock = { createPaymentApi.execute(id, xUser, request) })
     }
 }
